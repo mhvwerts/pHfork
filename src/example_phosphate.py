c53ad1f685169ec27a8e649a8895d83386c57caa
Martinus Werts <martinus.werts@ens-rennes.fr>
2023-05-29 14:05:54 +0200

    add more examples

---
+++ src/example_phosphate.py
@@ -0,0 +1,64 @@
+# Example calculations:
+# phosphate buffer system
+#
+#
+
+from pHcalc import Inert as IonAq
+from pHcalc import Acid as AcidAq
+from pHcalc import AcidGas as AcidGasEq
+from pHcalc import System
+
+# phosphate pH 7.4
+# H2PO4(-) <-> HPO4(2-) + H+
+# 
+# pKa = 7.20,  room temp, infinite dilution
+# pKa = 6.81, total phosphate = 0.1M
+# 
+#  see https://www.chem.fsu.edu/chemlab/Mastering/PhosphateBuffers.htm
+#
+# NaH2PO4.2H2O    156.01 g/mol
+# Na2HPO4.12H2O   358.14 g/mol
+#
+
+H2PO4 = {'charge' : -1,
+         'pKa'    : 6.9} # since we dilute below 0.1M,
+                         #  apparent pKa chosen to be slightly higher
+                         # TODO: be more precise about this!
+
+Na = {'charge' : +1}
+
+
+
+print('100 mol% NaH2PO4')
+comp1 = AcidAq(**H2PO4,
+               conc = 0.100)
+
+comp2 = IonAq(**Na,
+              conc = 0.100)
+
+buff = System(comp1, comp2)
+
+buff.pHsolve()
+print('pH = ',buff.pH)
+print()
+
+
+
+print('25 mol% NaH2PO4 + 75 mol% Na2HPO4')
+comp1 = AcidAq(**H2PO4,
+               conc = 0.100)
+
+comp2 = IonAq(**Na,
+              conc = 0.175)
+
+buff = System(comp1, comp2)
+
+buff.pHsolve()
+print('pH = ',buff.pH)
+print()
+
+
+
+print('for 0.1M stock (25 mol% NaH2PO4 + 75 mol% Na2HPO4): ')
+print(0.025 * 157.6, 'g/l NaH2PO4.2H2O')
+print(0.075 * 358.14, 'g/l Na2HPO4.12H2O')
---
+++ src/example_trisHCl.py
@@ -0,0 +1,57 @@
+# Example calculations:
+# Tris-HCl buffer system
+#
+#
+
+from pHcalc import Inert as IonAq
+from pHcalc import Acid as AcidAq
+from pHcalc import AcidGas as AcidGasEq
+from pHcalc import System
+
+
+# Tris
+# 
+# pKa = 8.07, T=298K
+# pKa = 8.20, T=293K
+#
+# Tris.HCl      157.60 g/mol, solubility 667 g/l
+# Tris base     121.14 g/mol, solubility 666 g/l (?)
+#
+
+TrisH = {'charge' : +1,
+           'pKa'      : 8.20}
+
+Cl = {'charge' : -1}
+
+
+print('100 mol% Tris-HCl')
+comp1 = AcidAq(**TrisH,
+               conc = 0.100)
+
+comp2 = IonAq(**Cl,
+              conc = 0.100)
+
+buff = System(comp1, comp2)
+
+buff.pHsolve()
+print('pH = ',buff.pH)
+print()
+
+
+print('85 mol% Tris-HCl + 15 mol% Tris free base')
+comp1 = AcidAq(**TrisH,
+               conc = 0.100)
+
+comp2 = IonAq(**Cl,
+              conc = 0.085)
+
+buff = System(comp1, comp2)
+
+buff.pHsolve()
+print('pH = ',buff.pH)
+print()
+
+print('for 0.1M stock (85 mol% Tris-HCl + 15 mol% Tris free base):')
+print(0.085 * 157.6, 'g/l Tris-HCl')
+print(0.015 * 121.14, 'g/l Tris base')
+
